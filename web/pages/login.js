a5b5a294de454af45cd6543a18b6e6c4a2493be0
Snuffy <rodrigo.sgarabotto.scola@gmail.com>
2023-02-24 22:35:10 -0300

    changed colors

--- web/pages/login.js
+++ web/pages/login.js
@@ -1,34 +1,34 @@
-import { useRouter } from "next/router"
-import { Text, Box, Input, Button, Center, Link, Heading, Flex } from "@chakra-ui/react"
-import { useSupabaseClient } from "@supabase/auth-helpers-react"
-import { useState } from "react"
-import { FormInput } from "../Components/inputs/FormInput"
-import { RenderHead } from "../Components/RenderHead"
-import { BASEURL } from "../utils/formatting/utils"
-import { BoxCard } from "../Components/cards/BoxCard"
-import formatter from "../utils/formatting/formatting"
-import { createNotification } from "../store/notifications/NotificationSlice"
+import { useRouter } from "next/router";
+import { Text, Box, Input, Button, Center, Link, Heading, Flex } from "@chakra-ui/react";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import { FormInput } from "../Components/inputs/FormInput";
+import { RenderHead } from "../Components/RenderHead";
+import { BASEURL } from "../utils/formatting/utils";
+import { BoxCard } from "../Components/cards/BoxCard";
+import formatter from "../utils/formatting/formatting";
+import { createNotification } from "../store/notifications/NotificationSlice";
 
 export default function LOGINPAGE() {
-	const [{ email, password }, setEmailPassword] = useState({ email: "", password: "" })
-	const supabase = useSupabaseClient()
+	const [{ email, password }, setEmailPassword] = useState({ email: "", password: "" });
+	const supabase = useSupabaseClient();
 	const handleChange = (e) => {
-		setEmailPassword((curr) => ({ ...curr, [e.target.name]: e.target.value }))
-	}
-	const router = useRouter()
+		setEmailPassword((curr) => ({ ...curr, [e.target.name]: e.target.value }));
+	};
+	const router = useRouter();
 	const handleSubmit = async (e) => {
-		e.preventDefault()
+		e.preventDefault();
 		const { error, data } = await supabase.auth.signInWithPassword({
 			email,
 			password,
-		})
+		});
 		if (error) {
-			createNotification({ title: error.message, status: "error" })
+			createNotification({ title: error.message, status: "error" });
 		}
 		if (data?.user?.id) {
-			router.push("/")
+			router.push("/");
 		}
-	}
+	};
 
 	return (
 		<Box>
@@ -39,10 +39,22 @@ export default function LOGINPAGE() {
 						<BoxCard gap={4} px={10}>
 							<Heading textAlign={"center"}>Login</Heading>
 							<FormInput w={"full"} py={4} labelText={"Email"}>
-								<Input name="email" onChange={handleChange} value={email} type={"email"} />
+								<Input
+									color={"black"}
+									name="email"
+									onChange={handleChange}
+									value={email}
+									type={"email"}
+								/>
 							</FormInput>
 							<FormInput w={"full"} labelText={"Password"}>
-								<Input name={"password"} onChange={handleChange} value={password} type={"password"} />
+								<Input
+									color={"black"}
+									name={"password"}
+									onChange={handleChange}
+									value={password}
+									type={"password"}
+								/>
 							</FormInput>
 							<Flex pt={4}>
 								<Button
@@ -66,5 +78,5 @@ export default function LOGINPAGE() {
 				</Box>
 			</form>
 		</Box>
-	)
+	);
 }
